ef36c08a43b8b0c6ee10c64abfdbdb16881dc82b
William <lecongthinh24062002@gmail.com>
2026-06-26 16:24:17 +0700

    reduce prisma version 5.22.0

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,8 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from 'generated/prisma/client';
-
+import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
     async onModuleInit() {
-        // Kết nối với database ngay khi ứng dụng được khởi động
         await this.$connect();
     }
 }
